ca970bb72fc069ed873fc24f184f3a40acb8aca5
Zumon Hossain <90811927+zumonhossain@users.noreply.github.com>
2025-12-27 22:35:47 +0900

    Directives: V-bind

---
+++ 3=Directives V-bind/app.js
@@ -0,0 +1,12 @@
+
+const app = Vue.createApp({
+    data() {
+        return {
+            src: "https://www.bigfootdigital.co.uk/wp-content/uploads/2020/07/image-optimisation-scaled.jpg",
+            alt: "image",
+            link: "https://github.com/zumonhossain"
+        }
+    }
+});
+
+app.mount('#app')
